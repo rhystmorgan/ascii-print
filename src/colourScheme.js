cc6b51ff9620fc5e99fa76599f676b3eab43027f
rhystmorgan <morgan.rhystomas@gmail.com>
2025-02-09 16:54:44 +0000

    refactor & lighten 'f'

---
+++ src/colourScheme.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+import { colours } from "./colours.js";
+import { chars } from "./chars.js";
+
+export function makeColourScheme(list) {
+  const reversed = list.split('').reverse()
+  const base = reversed[0]
+  const contrast = (reversed[2] === base) ? reversed[3] : reversed[2];
+
+  const colour1 = colours[base]
+  const colour2 = colours[contrast]
+
+  console.log(`ColourScheme: ${chalk.hex(colour1)(chars[base])} ${chalk.hex(colour2)(chars[contrast])}`)
+  return [colour1, colour2]
+}
